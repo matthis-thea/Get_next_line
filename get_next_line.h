9f989e6ca3b1471c234abe54d35a3bc80a873bd7
lamalicce <matthea834@gmail.com>
2022-12-02 11:59:02 +0100

    refait

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: haze <haze@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 08:45:58 by haze              #+#    #+#             */
-/*   Updated: 2022/12/01 13:11:53 by haze             ###   ########.fr       */
+/*   Updated: 2022/12/02 11:57:21 by haze             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,7 @@
 # include <unistd.h>
 # include <fcntl.h>
 # ifndef BUFFER_SIZE
-#	define BUFFER_SIZE (42)
+#	define BUFFER_SIZE (5)
 # endif 
 char	*get_next_line(int fd);
-char	*ft_read_line(int fd, char *old_string);
-char	*ft_strdup_newstring(char *old_string);
-char	*ft_get_end(char *old_string);
-int		ft_strlen(char *str);
-int		ft_strchr(char *string, char searchedChar);
-char	*ft_strdup(const char *source, int nombre);
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,62 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_utils.c                                   :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: haze <haze@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 08:53:39 by haze              #+#    #+#             */
-/*   Updated: 2022/12/01 13:15:21 by haze             ###   ########.fr       */
+/*   Updated: 2022/12/02 11:51:03 by haze             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i])
-		i++;
-	return (i);
-}
-
-
-int	ft_strchr(char *oldstring, char searchedChar)
-{
-	int	i;
-
-	i = 0;
-	while (oldstring[i] != '\0')
-	{
-		if (oldstring[i] == (char) searchedChar)
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-char	*ft_strdup(const char *source, int nombre)
-{
-	char	*str;
-	int		i;
-
-	str = NULL;
-	i = 0;
-	while (i < nombre)
-		i++;
-	str = (char *) malloc(sizeof(char) * (i + 1));
-	if (str == NULL)
-		return (0);
-	i = 0;
-	while (i < nombre)
-	{
-		str[i] = source[i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
